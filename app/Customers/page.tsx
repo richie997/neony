4729c89bc5ab440ff335585445f840828984b3de
richie997@ <obedsonmalonda@gmail.com>
2024-08-18 23:29:46 +0200

    change folder name

--- app/Customers/page.tsx
+++ app/Customers/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Customers = () => {
   return (
     <div>
-         Customers
+         <h1 className='text-2xl font-bold'>Customers</h1>
     </div>
   )
 }
 
-export default Customers
+export default Customers;
